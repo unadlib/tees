ca6871098427fb1c946d339d3b940f1741be5dd3
Harry.Hou <harry.hou@ringcentral.com>
2019-02-16 00:29:14 +0800

    :memo: (reporter) add readme file and quick fix time display issues

--- packages/tees-reporter/utils/helper.js
+++ packages/tees-reporter/utils/helper.js
@@ -50,8 +50,28 @@ const promiseReadfile = (path) => {
   })
 }
 
+const formatDate = (timesteamp) => {
+  const date = new Date(timesteamp);
+  let fmt = 'yy-MM-dd hh:mm:ss';
+  const o = {
+    "M+": date.getMonth() + 1,
+    "d+": date.getDate(), 
+    "h+": date.getHours(),
+    "m+": date.getMinutes(),
+    "s+": date.getSeconds(),
+    "q+": Math.floor((date.getMonth() + 3) / 3), 
+    "S": date.getMilliseconds()
+  };
+  if (/(y+)/.test(fmt))
+      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + ""));
+  for (var k in o)
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
+
 module.exports = {
   filterSkipTest,
   formatConsole,
   promiseReadfile,
+  formatDate
 };
