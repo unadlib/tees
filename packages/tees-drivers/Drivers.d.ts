bfabe70a367e8b728dd528d6ed2c16d2adaa2f85
summerGancf <summer.gan@lmxmn204.rcoffice.ringcentral.com>
2019-03-08 10:24:03 +0800

    docs(tees-driver): add driver api prompt

---
+++ packages/tees-drivers/Drivers.d.ts
@@ -0,0 +1,26 @@
+interface $ {
+    getText(selector: string, options?: Object): string
+    getNewOpenPage() : Page
+    clickToGetNewOpenPage(selector: string, browser: Browser, options ?: Object): Page
+    getTexts(selector: string, options?: Object): Array<string>
+    getAttribute(selector: string, attribute: string, options?: Object): string
+    getProperty(selector: string, property: string, options?: Object): string
+    getValue(selector: string, options: Object): string
+    html(selector: string): string
+    url(): sting
+    click(selector: string, options?: Object): string
+    type(selector: string, value: string, options?: Object): string
+    waitForSelector(selector: string, options?: Object): Promise<?ElementHandle>
+    waitForFrames(frameSelectors: string): Frame
+    waitForFunction(fn:function|string, ...args?: Serializable|JSHandle): void
+    screenshot({ path }: string): string
+    goto(url: string, options?: Object): void
+    execute(...args?: Serializable|JSHandle): Promise<Serializable>
+    clear(selector: string, options?: Object): void
+    $(selector: string, options?: Object): Promise<?ElementHandle>
+    $$(selector: string, options?: Object): Promise<Array<ElementHandle>>
+   }
+   interface $_Static {
+    (node: Object):$
+   }
+   declare var $:$_Static
