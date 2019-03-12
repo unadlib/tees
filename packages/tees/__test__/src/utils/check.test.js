d20a704a3dcbe2a357b1b6e10d0da707294d218f
chenxq <sarah.chen@ringcentral.com>
2019-03-12 16:10:05 +0800

    test(check.test.js): add UT to check.js (#15)
    * test(check.test.js): add UT to check.js

--- packages/tees/__test__/src/utils/check.test.js
+++ packages/tees/__test__/src/utils/check.test.js
@@ -6,8 +6,63 @@ const {
 
 describe('utils/check', () => {
   test('isNil', () => {
-    [undefined, null].forEach((item) => {
-      expect(isNil(item)).toBeTruthy();
-    })
+    [
+      undefined,
+      null
+    ].forEach((item) => {
+      (expect(isNil(item)).toBeTruthy());
+    });
+    expect(isNil()).toBeTruthy();
+    [
+      { test: 1 }, 
+      0, 
+      1, 
+      true, 
+      false, 
+      '', 
+      'test', 
+      {}, 
+      [], 
+      [1]
+    ].forEach((item) => {
+      (expect(isNil(item)).toBeFalsy());
+    });
+  })
+
+  test('isPlainobject', () => {
+    [
+      {}, 
+      { test: 1 }
+    ].forEach((item) => {
+      (expect(isPlainobject(item)).toBeTruthy());
+    });
+    [
+      1,
+      true,
+      false,
+      'test',
+      []
+    ].forEach((item) => {
+      (expect(isPlainobject(item)).toBeFalsy());
+    });
+    expect(isPlainobject()).toBeFalsy();
+  })
+
+  test('checkValidity', () => {
+    [
+      [],
+      undefined,
+      null
+    ].forEach((item) => {
+      (expect(checkValidity(item)).toEqual(['./']));
+    });
+    expect(checkValidity()).toEqual(['./']);
+    [
+      '',
+      './test/test',
+      '!@#$%^&*()_+-=<>,.?/:;"\'`~|\\}/{[]}'
+    ].forEach((item) => {
+      (expect(checkValidity(item)).toEqual(item));
+    });
   })
 });
