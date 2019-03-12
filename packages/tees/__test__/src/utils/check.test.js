871148cc5ee27ec8231abcba15e0aad3def5ccf0
chenxq <358009028@qq.com>
2019-03-12 10:09:43 +0800

    test(check.test.js): add UT to check.js

--- packages/tees/__test__/src/utils/check.test.js
+++ packages/tees/__test__/src/utils/check.test.js
@@ -6,8 +6,27 @@ const {
 
 describe('utils/check', () => {
   test('isNil', () => {
-    [undefined, null].forEach((item) => {
-      expect(isNil(item)).toBeTruthy();
+    [, undefined, null].forEach((item) => {
+      (expect(isNil(item)).toBeTruthy());
+    });
+    [{test: 1}, 1, {}, [], [1]].forEach((item) => {
+      (expect(isNil(item)).toBeFalsy());
+    });
+  })
+
+  test('isPlainobject', ()=> {
+    [{}, {test: 1}].forEach((item) => {
+      (expect(isPlainobject(item)).toBeTruthy());
+    });
+    [1, []].forEach((item) => {
+      (expect(isPlainobject(item)).toBeFalsy());
+    });
+  })
+
+  test('checkValidity', ()=> {
+    [, [], undefined, null].forEach((item) => {
+      (expect(checkValidity(item)).toEqual(['./']));
     })
+    expect(checkValidity('./test/test')).toEqual('./test/test');
   })
 });
