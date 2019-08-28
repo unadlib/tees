63102aeeff477b92bda75028176376dcfbadc1b6
summerGancf <summer.gan@lmxmn204.rcoffice.ringcentral.com>
2019-08-28 17:37:02 +0800

    test(E2E): clean code

--- example/loginInfo.js
+++
@@ -1,9 +0,0 @@
-const accounts = {
-    googleAccount: 'integration.jenkins@gmail.com',
-    googlePwd: 'Rcisfun23',
-    gmailInviteAccount: 'integration.ring@gmail.com',
-    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
-    userDataDir: '../../GoogleProfile/Chrome',
-    gmailOauthCredentialsPath: '../../gmailCredentials.json',
-    gmailOauthTokenPath: '../../gmailToken.json',
-  };
