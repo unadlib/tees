1215798b0366bec37f3150df989712788dcadc45
lenaChenv <avermouth@163.com>
2019-09-09 15:09:37 +0800

    feat(postSetup): Unit Test

--- packages/tees/src/lifecycle/postSetup.js
+++ packages/tees/src/lifecycle/postSetup.js
@@ -89,18 +89,9 @@ function getExecCaseParams({
     option,
     caseTag,
     tag
-  });
+  })
   const groupInfos = group.length > 0 ? `in ${group.join(' & ')} ` : '';
-  const _optionTags = Object.entries(_option)
-  .reduce((tags, [name, value]) => {
-    const isAccountTag = ['loginAccount', 'accounts'].includes(name);
-    if (!isAccountTag) return tags;
-    return [
-      ...tags,
-      `& ${name}-${value}`
-    ];
-  }, []).join(' ');
-  const tail = ` => (${project} ${groupInfos}${_optionTags}on ${driver})`;
+  const tail = ` => (${project} ${groupInfos}on ${driver})`;
   const caseTitle = `${name}${tail}`;
   const {
     config,
@@ -116,8 +107,9 @@ function getExecCaseParams({
     modes,
     isSandbox,
   });
+
   const context = {
-    logger: generateLogger(caseTitle, global.hasReporter, isVerbose),
+    logger: generateLogger(caseTitle, global.hasReporter),
     driver: instance.driver,
     get browser() {
       return context.driver.browser;
